9f434ffb94cd4795a7072455fff6d5e482b608cd
LAB-USR-CHIMBOTE <LAB-USR-CHIMBOTE@grupoutp.net>
2024-10-04 16:30:43 -0500

    Zdenko

---
+++ ConsultorioMedico/src/main/java/com/mycompany/consultoriomedico/ConsultorioMedico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.consultoriomedico;
+
+/**
+ *
+ * @author LAB-USR-CHIMBOTE
+ */
+public class ConsultorioMedico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
